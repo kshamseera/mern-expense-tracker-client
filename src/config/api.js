6f609dd893921f226d00eda39189fed557dfa10b
Shamseera <kurumbeths@gmail.com>
2020-07-25 16:39:41 +1000

    changed base url

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 //create axios instance
 export default axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://obscure-basin-70004.herokuapp.com/',
     timeout: 5000,
     withCredentials: true
     // 'https://obscure-basin-70004.herokuapp.com/'
